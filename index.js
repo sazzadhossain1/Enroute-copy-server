d5f6d149f1f6bf315ae37306aa4148db04a0b6b6
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-17 19:44:27 +0600

    server is runnint get and listen

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("assdfsdfsdf");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("MY SERVER IS RUNNING");
+});
+
+app.listen(port, () => {
+  console.log("Express app listening on port");
+});
