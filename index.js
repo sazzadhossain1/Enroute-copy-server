dcaf65511e525ccdb2925a335d7695d719c4b2f5
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-18 20:02:44 +0600

    insl cors create app api file with api and get api

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+const allApis = require("./api.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("MY SERVER IS RUNNING");
+});
+
+app.get("/allApis", (req, res) => {
+  res.send(allApis);
 });
 
 app.listen(port, () => {
